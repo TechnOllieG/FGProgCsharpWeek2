55b381edfac3f0df5687dc4d525b7969d78beb18
TechnOllieG <56596714+TechnOllieG@users.noreply.github.com>
2020-08-27 10:16:18 +0200

    Added scenes and scripts (forgot to push from school)

---
+++ Assets/Scripts/CounteractGravity/Hover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.AddForce(-Physics.gravity*_rigidbody.mass);
+    }
+}
